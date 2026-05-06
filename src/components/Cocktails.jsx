3a2a3435db85f712785dbda5c77d1a1db8b899b5
Jose <josepu03@gmail.com>
2026-05-06 15:00:33 -0500

    first commit

--- src/components/Cocktails.jsx
+++ src/components/Cocktails.jsx
@@ -3,65 +3,75 @@ import gsap from 'gsap';
 import { cocktailLists, mockTailLists } from '../../constants/index.js'
 
 const Cocktails = () => {
- useGSAP(() => {
-	const parallaxTimeline = gsap.timeline({
-	 scrollTrigger: {
-		trigger: '#cocktails',
-		start: 'top 30%',
-		end: 'bottom 80%',
-		scrub: true,
-	 }
-	})
-	
-	parallaxTimeline
-	 .from('#c-left-leaf', {
-		x: -100, y: 100
-	})
-	 .from('#c-right-leaf', {
-		x: 100, y: 100
-	})
- })
- 
- return (
-	<section id="cocktails" className="noisy">
-	 <img src="/images/cocktail-left-leaf.png" alt="l-leaf" id="c-left-leaf" />
-	 <img src="/images/cocktail-right-leaf.png" alt="r-leaf" id="c-right-leaf" />
-	 
-	 <div className="list">
-		<div className="popular">
-		 <h2>Most popular cocktails:</h2>
-		 
-		 <ul>
-			{cocktailLists.map(({ name, country, detail, price }) => (
-			 <li key={name}>
-				<div className="md:me-28">
-				 <h3>{name}</h3>
-				 <p>{country} | {detail}</p>
-				</div>
-				<span>- {price}</span>
-			 </li>
-			))}
-		 </ul>
-		</div>
-		
-		<div className="loved">
-		 <h2>Most loved mocktails:</h2>
-		 
-		 <ul>
-			{mockTailLists.map(({ name, country, detail, price }) => (
-			 <li key={name}>
-				<div className="me-28">
-				 <h3>{name}</h3>
-				 <p>{country} | {detail}</p>
-				</div>
-				<span>- {price}</span>
-			 </li>
-			))}
-		 </ul>
-		</div>
-	 </div>
-	</section>
- )
+  useGSAP(() => {
+    const parallaxTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: '#cocktails',
+        start: 'top 30%',
+        end: 'bottom 80%',
+        scrub: true,
+      }
+    })
+
+    parallaxTimeline
+      .from('#c-left-leaf', {
+        x: -100, y: 100
+      })
+      .from('#c-right-leaf', {
+        x: 100, y: 100
+      })
+  })
+
+  return (
+    <section id="cocktails" className="noisy">
+      <img src="/images/cocktail-left-leaf.png" alt="l-leaf" id="c-left-leaf" />
+      <img src="/images/cocktail-right-leaf.png" alt="r-leaf" id="c-right-leaf" />
+
+      <div className="list">
+        <div className="popular">
+          <h2>Actores Involucrados:</h2>
+
+          <ul>
+            <li>
+              <div className="md:me-28">
+                <h3>Exportador</h3>
+                <p>Perú | Perales Huancaruna S.A.C. (PERHUSA)</p>
+              </div>
+              <span>- Origen</span>
+            </li>
+            <li>
+              <div className="md:me-28">
+                <h3>Importador</h3>
+                <p>Alemania | Comprador en el mercado alemán</p>
+              </div>
+              <span>- Destino</span>
+            </li>
+          </ul>
+        </div>
+
+        <div className="loved">
+          <h2>Detalles del Proyecto:</h2>
+
+          <ul>
+            <li>
+              <div className="me-28">
+                <h3>Producto a exportar</h3>
+                <p>Café Peruano | En grano verde (variedad arábica)</p>
+              </div>
+              <span>- Producto</span>
+            </li>
+            <li>
+              <div className="me-28">
+                <h3>Ruta Logística</h3>
+                <p>Marítima | Salida Puerto del Callao - Destino Puerto de Hamburgo</p>
+              </div>
+              <span>- Ruta</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default Cocktails
